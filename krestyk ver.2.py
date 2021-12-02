1c3e88d7bb6329cd89c9f5755679cecf083db93b
TTawa88 <95316934+TTawa88@users.noreply.github.com>
2021-12-03 00:06:44 +0200

    Add files via upload

---
+++ krestyk ver.2.py
@@ -0,0 +1,70 @@
+import os
+print()
+print()
+print("*" * 90)
+print(" " * 30, " Игра Крестики-нолики")
+print("По традиции первым ходит Х")
+print("Что-бы сделать ход нужно ввести в размеченную ячейку цифру, от 0 до 9 для выбора места")
+print("*" * 90)
+print()
+input("Нажмите Enter для продолжения!")
+os.system('cls||clear')
+board = list(range(1, 10))
+
+def pole (board):
+   os.system('cls||clear')
+   print ("-" * 13)
+   for i in range(3):
+      print ("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+      print ("-" * 13)
+
+def take_input (player_token):
+   valid = False
+   while not valid:
+      player_answer = input("Ходит " + player_token+", ведите номер ячейки: ")
+      try:
+         player_answer = int(player_answer)
+      except:
+         print("Введите число!!!")
+         continue
+      if player_answer >= 1 and player_answer <= 9:
+         if (str(board[player_answer-1]) not in "XO"):
+            board[player_answer-1] = player_token
+            valid = True
+         else:
+            print("Эта клетка уже занята!")
+      else:
+        print("Введите число от 1 до 9.")
+
+def checkwin (board):
+   win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+   for each in win_coord:
+       if board[each[0]] == board[each[1]] == board[each[2]]:
+          return board[each[0]]
+   return False
+
+def main (board):
+    counter = 0
+    win = False
+    while not win:
+        pole(board)
+        if counter % 2 == 0:
+           take_input ("\033[31mX\033[0m")
+        else:
+           take_input ("\033[33mO\033[0m")
+        counter += 1
+        if counter > 4:
+           tmp = checkwin(board)
+           if tmp:
+              pole(board)
+              print (tmp, " выиграл!")
+              win = True
+              break
+        if counter == 9:
+            pole(board)
+            print ("Ничья!")
+            break
+
+main (board)
+
+input("Нажмите Enter для выхода!")
